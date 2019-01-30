2858a03292cf3b93c533a7c4536db69bdc667a0a
petercolella <peter.joseph.colella@gmail.com>
2019-01-29 21:24:22 -0500

    Updates

---
+++ precache-manifest.bef3adf3ba86caa1ea65fa2927d30bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4065e7811f526bf4236f",
+    "url": "/clicky-game-solution/static/css/main.3c88c623.chunk.css"
+  },
+  {
+    "revision": "4065e7811f526bf4236f",
+    "url": "/clicky-game-solution/static/js/main.4065e781.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/clicky-game-solution/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "d32b4ad0ada4e105d300",
+    "url": "/clicky-game-solution/static/js/runtime~main.d32b4ad0.js"
+  },
+  {
+    "revision": "4c1628c003f97a18c7761a67cb004206",
+    "url": "/clicky-game-solution/index.html"
+  }
+];
